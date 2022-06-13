04ff7ebf479d197215e8e0f4d5a41c805b582220
andywtse <atse4995@gmail.com>
2022-06-13 16:07:38 -0400

    Initializes repo

---
+++ data/students-data.js
@@ -0,0 +1,9 @@
+const students = [
+  {text: 'David', _id: 123001, horse: true},
+  {text: 'Ben', _id: 123002, horse: false},
+  {text: 'Michael', _id: 123003, horse:true},
+]
+
+export { 
+	students
+}
---
+++ server.js
@@ -0,0 +1,54 @@
+// import modules
+import express from 'express'
+// import the todo data
+import { todos } from './data/todo-data.js'
+import { students } from './data/students-data.js';
+// we could also do:
+// import * as todoData from './data/todo-data.js'
+// but not both! (well, you can, but please don't)
+
+// Create Express app
+
+const app = express();
+
+// Configure the app (app.set)
+app.set('view engine', 'ejs');
+
+
+// Mount Middleware (app.use)
+
+
+// Mount routes
+app.get('/', function(req,res) {
+  res.send('<h1>hello, all my friends</h>')
+});
+
+app.get('/home', function(req,res){
+  res.render('home');
+});
+
+app.get('/cars', function(req, res) {
+	res.send('Here is a list of my cars...')
+})
+
+app.post('/cars', function(req, res) {
+	res.send('Thanks for the new car!')
+})
+
+app.get('/todos', function(req, res) {
+  res.render('todos/index', {
+    todos: todos
+  })
+})
+
+app.get('/students', function(req, res) {
+  res.render('students/index', {
+    students:students
+  })
+})
+
+// Tell the app to listen on port 3000
+
+app.listen(3000, function() {
+  console.log('Listening on port 3000')
+})
